642025bbb067fbca72b51e9c471e8e90138cfbe0
sena <sena@pop-os.localdomain>
2023-03-11 23:39:39 -0300

    adding challenges

---
+++ sql/weather-observation-station-6.sql
@@ -0,0 +1,9 @@
+-- Weather Observation Station 6
+
+-- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION.
+-- Your result cannot contain duplicates.
+-- The STATION table is described as follows:
+
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE CITY REGEXP '^[aeiou]';
