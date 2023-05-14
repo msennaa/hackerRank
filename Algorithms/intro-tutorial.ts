ee8d0ea00191a769015b6dfb7bf429083c73b061
Matheus Sena <senamatheusjob@gmail.com>
2023-05-13 23:16:19 -0300

    changing to typescript

--- Algorithms/intro-tutorial.ts
+++ Algorithms/intro-tutorial.ts
@@ -1,4 +1,4 @@
-function introTutorial(V, arr) {
+function introTutorial(V: number, arr: number[]) {
     for(let index = 0; index < arr.length; index += 1) {
         if(arr[index] === V) {
             return index;
