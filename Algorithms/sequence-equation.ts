36d1547c84b409807fe32c459c1c9d8e1cb45a91
Matheus Sena <senamatheusjob@gmail.com>
2023-05-14 22:11:58 -0300

    Changin to typescript

--- Algorithms/sequence-equation.ts
+++ Algorithms/sequence-equation.ts
@@ -1,4 +1,4 @@
-function permutationEquation(p) {
+function permutationEquation(p: number[]) {
     const result = new Array(p.length);
     for(const index of p) {
         result[p[p[index - 1]-1]-1] = index;
